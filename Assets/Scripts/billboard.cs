16f85484ef75d47d37cea3a02b8ea119f69aea99
ManHunter <bymotorsinfo@gmail.com>
2015-08-16 21:21:21 +0300

    Replace Update on LateUpdate function

--- Assets/Scripts/billboard.cs
+++ Assets/Scripts/billboard.cs
@@ -11,39 +11,43 @@ using System.Collections;
 
 public class billboard : MonoBehaviour
 {
-	public Camera m_Camera;
-	public bool amActive =false;
-	public bool autoInit =false;
-	GameObject myContainer;	
-	
-	void Awake(){
-		if (autoInit == true){
-			m_Camera = Camera.main;
-			amActive = true;
-		}
-		
-		myContainer = new GameObject();
-		myContainer.name = "GRP_"+transform.gameObject.name;
-		myContainer.transform.position = transform.position;
-		transform.parent = myContainer.transform;
-	}
-	
-	
-	void Update(){
-		if(amActive==true){
-			
+    public Camera m_Camera;
+    public bool amActive = false;
+    public bool autoInit = false;
+    GameObject myContainer;
+
+    void Awake()
+    {
+        if (autoInit == true)
+        {
+            m_Camera = Camera.main;
+            amActive = true;
+        }
+
+        myContainer = new GameObject();
+        myContainer.name = "GRP_" + transform.gameObject.name;
+        myContainer.transform.position = transform.position;
+        transform.parent = myContainer.transform;
+    }
 
 
-				Vector3 v = new Vector3();
-				v.x = m_Camera.transform.position.x;
-				v.y = myContainer.transform.position.y;
-				v.z = m_Camera.transform.position.z;
-				myContainer.transform.LookAt(v);
+    void LateUpdate()
+    {
+        if (amActive == true)
+        {
 
-			
-			
-			
-			//myContainer.transform.LookAt(myContainer.transform.position + m_Camera.transform.rotation * Vector3.back, m_Camera.transform.rotation * Vector3.up);
-		}
-	}
+
+
+            Vector3 v = new Vector3();
+            v.x = m_Camera.transform.position.x;
+            v.y = myContainer.transform.position.y;
+            v.z = m_Camera.transform.position.z;
+            myContainer.transform.LookAt(v);
+
+
+
+
+            //myContainer.transform.LookAt(myContainer.transform.position + m_Camera.transform.rotation * Vector3.back, m_Camera.transform.rotation * Vector3.up);
+        }
+    }
 }
