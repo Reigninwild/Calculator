b5fa270e1e1cbf038db90ea0c42df99177ea454f
ManHunter <bymotorsinfo@gmail.com>
2015-08-29 19:31:23 +0300

    Arms texture and material Add Sound/bushes.mp3 Add BushesSound.cs

---
+++ Assets/Scripts/BushesSound.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BushesSound : MonoBehaviour
+{
+    public AudioClip sound;
+    AudioSource audio;
+
+
+    public void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.clip = sound;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Player"))
+            audio.Play();
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+            audio.Play();
+    }
+}
